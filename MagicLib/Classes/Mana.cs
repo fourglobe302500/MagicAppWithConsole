c3dffb5a64a5759b600545adf52886ee69b8003b
fourglobe302500 <franciscomiguelolima@gmail.com>
2020-11-15 18:21:17 +0000

    Adding the Mana Class

---
+++ MagicLib/Classes/Mana.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagicLib
+{
+    /// <summary>
+    /// Represents an Mana
+    /// </summary>
+    public class Mana
+    {
+        /// <summary>
+        /// Represents the Mana Type
+        /// </summary>
+        public ManaType Type { get; set; }
+    }
+}
