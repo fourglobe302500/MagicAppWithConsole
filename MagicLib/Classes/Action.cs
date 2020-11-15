26e1948841f73478a4b1761ca6ff47b2db7f4082
fourglobe302500 <franciscomiguelolima@gmail.com>
2020-11-15 18:20:28 +0000

    Adding the Action Class

---
+++ MagicLib/Classes/Action.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace MagicLib
+{
+    /// <summary>
+    /// Represents an Action that will happen
+    /// once certin conditions are met
+    /// </summary>
+    public class Action
+    {
+        /// <summary>
+        /// A List of all the conditions that must be met to the 
+        /// Action to occur
+        /// </summary>
+        public List<ActionType> Conditions { get; set; }
+
+        /// <summary>
+        /// A List of all Effects that will happen once the conditions
+        /// are met
+        /// </summary>
+        public List<Modifier> Effects { get; set; }
+    }
+}
