f85fca31179a55dd352e36375fb7c96639c912d2
fourglobe302500 <franciscomiguelolima@gmail.com>
2020-11-15 18:19:15 +0000

    Adding the Interface Card

---
+++ MagicLib/Classes/ICard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MagicLib
+{
+    /// <summary>
+    /// Represents an Card Interface
+    /// </summary>
+    public interface ICard
+    {
+        List<CardColor> CardColors { get; }
+        string Name { get; }
+        List<CardType> Types { get; }
+        CardRarity Rarity { get; }
+        string Image { get; }
+    }
+}
