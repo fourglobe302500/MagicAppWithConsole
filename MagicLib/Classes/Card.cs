99ca6cc7bc387f8e4f654299e26cf731a6f10d26
fourglobe302500 <franciscomiguelolima@gmail.com>
2020-11-15 21:27:38 +0000

    Changing ICard to Card

---
+++ MagicLib/Classes/Card.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MagicLib
+{
+    public abstract class Card
+    {
+        public abstract List<CardColor> CardColors { get; set; }
+        public abstract string Name { get; set; }
+        public abstract List<CardType> Types { get; set; }
+        public abstract CardRarity Rarity { get; set; }
+        public abstract string Image { get; set; }
+        public abstract string Description { get; set; }
+        public abstract List<ManaType> Cost { get; set; }
+        public abstract List<Action> Actions { get; set; }
+    }
+}
