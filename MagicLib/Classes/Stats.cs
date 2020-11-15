ca715b26fb43d2e1a3862fd1c35e2d8f8553dc79
fourglobe302500 <franciscomiguelolima@gmail.com>
2020-11-15 18:20:15 +0000

    Adding the Stats Class

---
+++ MagicLib/Classes/Stats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace MagicLib
+{
+    /// <summary>
+    /// Represents the satatus of an Card
+    /// </summary>
+    public class Stats
+    {
+        /// <summary>
+        /// Represents the base attack power the Creature
+        /// will have
+        /// </summary>
+        public int Power { get; set; }
+
+        /// <summary>
+        /// Represents the base health points the creature 
+        /// will have
+        /// </summary>
+        public int HP { get; set; }
+
+        /// <summary>
+        /// Is a List of Modifiers for the stats
+        /// </summary>
+        public List<Modifier> Modifiers { get; set; }
+    }
+}
